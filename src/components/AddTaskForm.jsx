3f790d3ec5152745de152d865348881f388f61ba
Oleg Shtein <olegshteyn@gmail.com>
2025-09-08 00:59:56 +0700

    feat: add focus to new task input and scroll to first incomplete task

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -6,6 +6,7 @@ const AddTaskForm = (props) => {
     addTask,
     newTaskTitle,
     setNewTaskTitle,
+    newTaskInputRef,
   } = props
 
   const onSubmit = (event) => {
@@ -24,6 +25,7 @@ const AddTaskForm = (props) => {
         id="new-task"
         value={newTaskTitle}
         onInput={(event) => setNewTaskTitle(event.target.value)}
+        ref={newTaskInputRef}
       />
       <Button type="submit">Add</Button>
     </form>
