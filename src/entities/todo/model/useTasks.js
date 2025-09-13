d6664dc6f571fa7010486d7257abfbbd2fd106bd
Oleg Shtein <olegshteyn@gmail.com>
2025-09-14 01:51:06 +0700

    refactor(tasks): migrate to useReducer and optimize TasksContext rerenders

--- src/entities/todo/model/useTasks.js
+++ src/entities/todo/model/useTasks.js
@@ -1,8 +1,35 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState, useReducer } from 'react'
 import tasksAPI from '@/shared/api/tasks'
 
+const tasksReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_ALL': {
+      return Array.isArray(action.tasks) ? action.tasks : state
+    }
+    case 'ADD': {
+      return [...state, action.task]
+    }
+    case 'TOGGLE_COMPLETE': {
+      const { id, isDone } = action
+
+      return state.map((task) => {
+        return task.id === id ? { ...task, isDone } : task
+      })
+    }
+    case 'DELETE': {
+      return state.filter((task) => task.id !== action.id)
+    }
+    case 'DELETE_ALL': {
+      return []
+    }
+    default: {
+      return state
+    }
+  }
+}
+
 const useTasks = () => {
-  const [tasks, setTasks] = useState([])
+  const [tasks, dispatch] = useReducer(tasksReducer, [])
 
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
@@ -15,9 +42,8 @@ const useTasks = () => {
     const isConfirmed = confirm('Are you sure you want to delete all?')
 
     if (isConfirmed) {
-      setTasks([])
-
-      tasksAPI.deleteAll(tasks).then(() => setTasks([]))
+      tasksAPI.deleteAll(tasks)
+        .then(() => dispatch({ type: 'DELETE_ALL' }))
     }
   }, [tasks])
 
@@ -26,28 +52,18 @@ const useTasks = () => {
       .then(() => {
         setDisappearingTaskId(taskId)
         setTimeout(() => {
-          setTasks(
-            tasks.filter((task) => task.id !== taskId)
-          )
+          dispatch({ type: 'DELETE', id: taskId })
           setDisappearingTaskId(null)
         }, 400)
       })
-  }, [tasks])
+  }, [])
 
   const toggleTaskComplete = useCallback((taskId, isDone) => {
     tasksAPI.toggleComplete(taskId, isDone)
       .then(() => {
-        setTasks(
-          tasks.map((task) => {
-            if (task.id === taskId) {
-              return { ...task, isDone }
-            }
-
-            return task
-          })
-        )
+        dispatch({ type: 'TOGGLE_COMPLETE', id: taskId, isDone })
       })
-  }, [tasks])
+  }, [])
 
   const addTask = useCallback((title) => {
     const newTask = {
@@ -57,7 +73,7 @@ const useTasks = () => {
 
     tasksAPI.add(newTask)
       .then((addedTask) => {
-        setTasks((prevTasks) => [...prevTasks, addedTask])
+        dispatch({ type: 'ADD', task: addedTask })
         setNewTaskTitle('')
         setSearchQuery('')
         newTaskInputRef.current.focus()
@@ -71,7 +87,10 @@ const useTasks = () => {
   useEffect(() => {
     newTaskInputRef.current.focus()
 
-    tasksAPI.getAll().then(setTasks)
+    tasksAPI.getAll()
+      .then((serverTasks) => {
+        dispatch({ type: 'SET_ALL', tasks: serverTasks })
+      })
   }, [])
 
   const filteredTasks = useMemo(() => {
